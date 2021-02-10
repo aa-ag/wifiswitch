d67e3d3ce93f80ac0c51d5b9ac1c5278e1b1ee27
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-09 21:16:38 -0600

    added enable_wifi_service()

--- script.py
+++ script.py
@@ -5,9 +5,15 @@ import subprocess
 ###--- FUNCTIONS ---###
 def disable_wifi_service():
     subprocess.run(
+        ["sudo", "networksetup", "-setnetworkserviceenabled", "Wi-Fi", "off"])
+
+
+def enable_wifi_service():
+    subprocess.run(
         ["sudo", "networksetup", "-setnetworkserviceenabled", "Wi-Fi", "on"])
 
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
     disable_wifi_service()
+    # enable_wifi_service()
