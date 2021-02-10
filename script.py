a87feb65b770220b7c50be50c9f10294777c28ba
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-10 16:54:52 -0600

    added on

--- script.py
+++ script.py
@@ -18,8 +18,14 @@ def turn_airplane_mode_off():
         ["sudo", "networksetup", "-setairportpower", "airport", "off"])
 
 
+def turn_airplane_mode_on():
+    subprocess.run(
+        ["sudo", "networksetup", "-setairportpower", "airport", "on"])
+
+
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
     # disable_wifi_service()
     # enable_wifi_service()
-    turn_airplane_mode_off()
+    # turn_airplane_mode_off()
+    turn_airplane_mode_on()
